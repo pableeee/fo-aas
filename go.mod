2b3b77cb31cd175dee630bc6e0da8a0d9dc1ce21
pablo.duco <pablo.duco@wildlifestudios.com>
2022-03-30 11:25:26 -0300

    adds generic server

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -17,11 +18,15 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/oklog/run v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ pkg/api/server.go
@@ -0,0 +1,147 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/pableeee/fo-aas/pkg/foaas"
+	"github.com/pableeee/fo-aas/pkg/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+)
+
+type Server struct {
+	router  *mux.Router
+	logger  *log.Logger
+	config  *Config
+	handler http.Handler
+}
+
+type Config struct {
+	HTTPServerTimeout         time.Duration
+	HTTPServerShutdownTimeout time.Duration
+	Host                      string
+	Port                      string
+	Hostname                  string
+	RateLimit                 time.Duration
+}
+
+func NewServer(c *Config, l *log.Logger) *Server {
+	return &Server{
+		router: mux.NewRouter(),
+		logger: l,
+		config: c,
+	}
+}
+
+func (s *Server) registerHandlers(ctx context.Context) {
+	// observability endpoints.
+	s.router.Handle("/metrics", promhttp.Handler())
+	s.router.HandleFunc("/ping", s.pingHandler).Methods("GET")
+
+	// domain router, to aviod tracking the operative endpoints above.
+	domainSubrouter := s.router.PathPrefix("").Subrouter()
+
+	s.registerMiddlewares(domainSubrouter)
+
+	// register domain endpoints
+	domainSubrouter.HandleFunc("/message", foaas.NewHandler()).Methods("GET")
+}
+
+func (s *Server) JSONResponse(w http.ResponseWriter, r *http.Request, result interface{}, responseCode int) {
+	body, err := json.Marshal(result)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(responseCode)
+	_, _ = w.Write(body)
+}
+
+func (s *Server) registerMiddlewares(router *mux.Router) {
+	// logging middleware
+	router.Use(
+		func(h http.Handler) http.Handler {
+			return handlers.LoggingHandler(os.Stdout, h)
+		},
+	)
+
+	// metrics middleware
+	prom := middleware.PrometheusMetricsMiddleware(s.config.Hostname)
+	router.Use(prom.Handler)
+
+	// rate limiting middleware
+	limiter := middleware.NewRateLimiterMiddleware(int(s.config.RateLimit))
+	router.Use(limiter.Handler)
+}
+
+func (s *Server) ListenAndServe(ctx context.Context) error {
+	// go s.startMetricsServer()
+
+	s.registerHandlers(ctx)
+
+	s.handler = s.router
+
+	// create the http server
+	srv := s.startServer()
+
+	// wait for context to be done
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), s.config.HTTPServerShutdownTimeout)
+	defer cancel()
+
+	s.logger.Info("Shutting down HTTP/HTTPS server")
+
+	// determine if the http server was started
+	if srv != nil {
+		if err := srv.Shutdown(ctx); err != nil {
+			s.logger.Warn(fmt.Sprintf("HTTP server graceful shutdown failed %s", err.Error()))
+
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Server) startServer() *http.Server {
+	// determine if the port is specified
+	if s.config.Port == "0" {
+		// move on immediately
+		return nil
+	}
+
+	srv := &http.Server{
+		Addr:         s.config.Host + ":" + s.config.Port,
+		WriteTimeout: s.config.HTTPServerTimeout,
+		ReadTimeout:  s.config.HTTPServerTimeout,
+		IdleTimeout:  2 * s.config.HTTPServerTimeout,
+		Handler:      s.handler,
+	}
+
+	// start the server in the background
+	go func() {
+		s.logger.Info("Starting HTTP Server ", fmt.Sprintf("addr:%s", srv.Addr))
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			s.logger.Fatal(fmt.Sprintf("HTTP server crashed %s", err.Error()))
+		}
+	}()
+
+	// return the server and routine
+	return srv
+}
+
+func (s *Server) pingHandler(w http.ResponseWriter, r *http.Request) {
+	s.JSONResponse(w, r, map[string]string{"status": "OK"}, http.StatusOK)
+}
